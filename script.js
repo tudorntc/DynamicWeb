7638ae0e74758e0d0f98c7e4eb38b1b38456337f
Nica Tudor <zatalo.nox@gmail.com>
2023-04-25 12:50:43 +0300

    Changing the p on click

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 function firstButton(){
     fib = document.getElementById("fb");
     fip = document.getElementById("fp");
+    fip.innerHTML  = "Incorrect";
 };
 
 function secondButton(){
     sib = document.getElementById("sb");
     sip = document.getElementById("sp");
+    sip.innerHTML  = "Correct";
 };
 
 function thirdButton(){
     tib = document.getElementById("tb");
     tip = document.getElementById("tp");
+    tip.innerHTML  = "Incorrect";
 };
