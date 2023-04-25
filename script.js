6a00bef1cfde31e440eeb79320ab3a2b875b198b
Nica Tudor <zatalo.nox@gmail.com>
2023-04-25 12:45:15 +0300

    Adding the p tags to js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function firstButton(){
     fib = document.getElementById("fb");
+    fip = document.getElementById("fp");   
 };
 
 function secondButton(){
     sib = document.getElementById("sb");
+    sip = document.getElementById("sp");
 };
 
 function thirdButton(){
     tib = document.getElementById("tb");
+    tip = document.getElementById("tp");
 };
