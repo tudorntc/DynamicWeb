d4363514dc92fdb142ddf9f4a283cdfdaaa32e48
Nica Tudor <zatalo.nox@gmail.com>
2023-04-25 12:42:07 +0300

    Function for every button

---
+++ script.js
@@ -0,0 +1,11 @@
+function firstButton(){
+    fib = document.getElementById("fb");
+}
+
+function secondButton(){
+    sib = document.getElementById("sb");
+}
+
+function thirdButton(){
+    tib = document.getElementById("tb");
+}
