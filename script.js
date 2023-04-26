606c4384816c46086b483eacb31e363216a0db52
Nica Tudor <zatalo.nox@gmail.com>
2023-04-26 09:48:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-function choice(){
-    function firstButton(){
-        fib = document.getElementById("fb");
-        fip = document.getElementById("fp");
-        fip.innerHTML  = "Incorrect";
-    };
+function firstButton(){
+    fib = document.getElementById("fb");
+    fip = document.getElementById("fp");
+    fip.innerHTML  = "Incorrect";
+};
 
-    function secondButton(){
-        sib = document.getElementById("sb");
-        sip = document.getElementById("sp");
-        sip.innerHTML  = "Correct";
-    };
+function secondButton(){
+    sib = document.getElementById("sb");
+    sip = document.getElementById("sp");
+    sip.innerHTML  = "Correct";
+};
 
-    function thirdButton(){
-        tib = document.getElementById("tb");
-        tip = document.getElementById("tp");
-        tip.innerHTML  = "Incorrect";
-    };
+function thirdButton(){
+    tib = document.getElementById("tb");
+    tip = document.getElementById("tp");
+    tip.innerHTML  = "Incorrect";
 };
